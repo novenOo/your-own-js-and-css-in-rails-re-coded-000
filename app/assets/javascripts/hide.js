acf71115bcd0b4e995e70b277249704037a34349
Noven Temer <noven5823@gmail.com>
2017-10-29 02:07:50 +0300

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,12 @@
+
+	$("#hide_this").click(function hideWhenClicked(event){
+		$("#hide_this").hide();
+	});
+
+
+
+// $( "#hide_this" ).click(function( event ) {
+// 	$( "#hide_this" ).hide();
+// 	$("body").toggleClass();
+//   // event.preventDefault();
+// });
